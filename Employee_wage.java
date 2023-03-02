6b5393ab35dbc30428d78b01262c5bd35a9a0f26
deepm24 <deep60640@gmail.com>
2023-03-02 20:22:06 +0530

    Calculating Wages for a Month

--- Employee_wage.java
+++ Employee_wage.java
@@ -6,29 +6,46 @@ class Employee_wage
 {
     public void Wage()
     {
-        int perHourWage = 20;
-        int fullTimeHour = 8;
-        int partTimeHour = 4;
+        int perHourWage=20;
+        int fullTimeHour=8;
+        int partTimeHour=4;
         int fullDayWage;
         int halfDayWage;
+        int workingDay=1;
+        int monthDay=1;
+        int monthlyWage=0;
+        int Absent=0;
+        Random rn=new Random();
 
-        Random rn = new Random();
-        int check = rn.nextInt(3);
+        while(workingDay!=20 && monthDay!=30)
+        {
+            int check=rn.nextInt(3);
 
-        switch (check) {
-            case 0:
-                fullDayWage = perHourWage * fullTimeHour;
-                System.out.println("Employee is Present for Full Day \nFullday wage:" + fullDayWage);
-                break;
-            case 1:
-                halfDayWage = perHourWage * partTimeHour;
-                System.out.println("Employee is Present for Half Day \nHalfday wage:" + halfDayWage);
-                break;
-            case 2:
-                System.out.println("Employee is Absent\nDaily wage is: 0");
-                break;
+            switch (check)
+            {
+                case 0:
+                    fullDayWage = perHourWage * fullTimeHour;
+                    monthlyWage += fullDayWage;
+                    monthDay ++;
+                    workingDay ++;
+                    break;
+                case 1:
+                    halfDayWage = perHourWage * partTimeHour;
+                    monthlyWage+=halfDayWage;
+                    monthDay++;
+                    workingDay++;
+                    break;
+                case 2:
+                    Absent ++;
+                    monthDay++;
+                    break;
 
+            }
         }
+        System.out.println("Monthly Wage :"+ monthlyWage);
+        System.out.println("Absent in Month :"+ Absent);
+        System.out.println("Working Day :"+ workingDay);
+        System.out.println("MonthDay :"+ monthDay);
 
     }
 }
