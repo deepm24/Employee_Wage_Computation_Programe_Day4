ac3fbadb56e5062acca86de61ab791209b695bf5
deepm24 <deep60640@gmail.com>
2023-03-02 20:06:01 +0530

    Modified: If Else To Switch Case Statement

--- Employee_wage.java
+++ Employee_wage.java
@@ -3,28 +3,32 @@ package org.example;
 import java.util.Random;
 
 class Employee_wage
- {
-     public  void Wage()
-     {
-         int perHourWage=20;
-         int fullTimeHour=8;
-         int partTimeHour=4;
-         int perDayWage;
-         Random rn=new Random();
-         int a=rn.nextInt(3);
-         if (a==0)
-         {
-             perDayWage=perHourWage*fullTimeHour;
-             System.out.println("Employee is Present for full day \nDaily wage:"+perDayWage);
-         }
-         else if (a==1)
-         {
-             perDayWage=perHourWage*partTimeHour;
-             System.out.println("Employee is Present for half day \nDaily wage:"+perDayWage);
-         }
-         else
-         {
-             System.out.println("Employee is Absent\nDaily wage is: 0");
-         }
-     }
- }
+{
+    public void Wage()
+    {
+        int perHourWage = 20;
+        int fullTimeHour = 8;
+        int partTimeHour = 4;
+        int fullDayWage;
+        int halfDayWage;
+
+        Random rn = new Random();
+        int check = rn.nextInt(3);
+
+        switch (check) {
+            case 0:
+                fullDayWage = perHourWage * fullTimeHour;
+                System.out.println("Employee is Present for Full Day \nFullday wage:" + fullDayWage);
+                break;
+            case 1:
+                halfDayWage = perHourWage * partTimeHour;
+                System.out.println("Employee is Present for Half Day \nHalfday wage:" + halfDayWage);
+                break;
+            case 2:
+                System.out.println("Employee is Absent\nDaily wage is: 0");
+                break;
+
+        }
+
+    }
+}
